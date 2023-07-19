047ce6c226164e34127771bc244a50092273ee41
addisu30 <dabaleaddisu21@outlook.com>
2023-07-19 09:27:54 +0300

    stats

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -4,7 +4,7 @@
 import sys
 
 cache = {'200': 0, '301': 0, '400': 0, '401': 0,
-                 '403': 0, '404': 0, '405': 0, '500': 0}
+         '403': 0, '404': 0, '405': 0, '500': 0}
 total_size = 0
 counter = 0
 
@@ -16,15 +16,15 @@ try:
             size = int(line_list[-1])
             if code in cache.keys():
                 cache[code] += 1
-                total_size += size
-                counter += 1
+            total_size += size
+            counter += 1
 
-if counter == 10:
-    counter = 0
-    print('File size: {}'.format(total_size))
-    for key, value in sorted(cache.items()):
-        if value != 0:
-            print('{}: {}'.format(key, value))
+        if counter == 10:
+            counter = 0
+            print('File size: {}'.format(total_size))
+            for key, value in sorted(cache.items()):
+                if value != 0:
+                    print('{}: {}'.format(key, value))
 
 except Exception as err:
     pass
